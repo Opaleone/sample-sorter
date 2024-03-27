9cb0d765e16a079d41b0e9a74e1bc55851bd972d
Leone Varando <lvarando@gmail.com>
2024-03-27 18:11:18 -0500

    Added regex for sample find

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import fs from 'fs';
 import path from 'path';
 
+let variableRegex = /[a-zA-Z-_\d]{0,256}snare[a-zA-Z-_\d]{0,256}/g;
+
 fs.mkdir(path.join('./', 'samples'), e => {
   if (e) return console.error(e);
   else console.log('Directory successfully created.');
