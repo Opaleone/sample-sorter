a4c8015e8a62475ee38ba758fe58361e8b543dfb
Leone Varando <lvarando@gmail.com>
2024-03-28 17:36:11 -0500

    Created ability to retrieve info about folders

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import fs from 'fs';
 import path from 'path';
+import getDirectories from './utils/getDirectories.js';
+import utils from './config.js';
 
 let variableRegex = /[a-zA-Z-_\d]{0,256}snare[a-zA-Z-_\d]{0,256}/g;
 
@@ -7,3 +9,7 @@ fs.mkdir(path.join('./', 'samples'), e => {
   if (e) return console.error(e);
   else console.log('Directory successfully created.');
 })
+
+const folders = await getDirectories(utils.folderPath);
+
+console.log(folders);
