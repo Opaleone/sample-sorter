626a456278dbecbb8fb47e598bea6b55e1fe00b2
Leone Varando <lvarando@gmail.com>
2024-03-28 17:35:17 -0500

    Abstracted function to external file for widespread use

---
+++ utils/getDirectories.js
@@ -0,0 +1,8 @@
+import { readdir } from 'fs/promises'
+
+const getDirectories = async source =>
+  (await readdir(source, { withFileTypes: true }))
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent)
+
+export default getDirectories
