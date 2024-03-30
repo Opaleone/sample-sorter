93165b65d57bb47da113c4d440186a8fb538830f
Leone Varando <lvarando@gmail.com>
2024-03-29 23:35:02 -0500

    Pushing for work on another comp

--- index.js
+++ index.js
@@ -3,13 +3,23 @@ import path from 'path';
 import getDirectories from './utils/getDirectories.js';
 import utils from './config.js';
 
-let testRegex = /[a-zA-Z-_\d]{0,256}(hat|hihat|HH)[a-zA-Z-_\d]{0,256}/gi;
+let testRegex = /([a-zA-Z-_\d])+(hat|hihat|HH)([a-zA-Z-_\d])+/gi;
 
-fs.mkdir(path.join('./', 'samples'), e => {
-  if (e) return console.error(e);
-  else console.log('Directory successfully created.');
-})
+// fs.mkdir(path.join('./', 'samples'), e => {
+//   if (e) return console.error(e);
+//   else console.log('Directory successfully created.');
+// })
 
 const folders = await getDirectories(utils.folderPath);
 
-console.log(folders);
+
+for (let i = 0; i < 1; i++) {
+  let currentDir = folders[i].path;
+  currentDir += `\\${folders[i].name}`
+
+  const dir = fs.readdirSync(currentDir, e => {
+    console.log(e)
+  })
+
+  console.log(dir);
+}
